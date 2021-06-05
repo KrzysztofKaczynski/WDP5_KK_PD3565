6eea95638a500fd4cf12ed08f09502e7f0d04392
Krzysztof <qris70@gmail.com>
2021-06-05 21:33:19 +0200

    add zad2

--- src/zad2/Main.java
+++ src/zad2/Main.java
@@ -9,6 +9,25 @@ package zad2;
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
+
+
+    ListaTowarow lt = new ListaTowarow();
+    ThreadA threadA = new ThreadA(lt);
+    ThreadB threadB = new ThreadB(lt);
+    threadA.start();
+    threadB.start();
+    try {
+      threadA.join();
+    } catch (InterruptedException e) {
+      System.out.println(e);
+    }
+    try {
+      threadB.join();
+    } catch (InterruptedException e) {
+      System.out.println(e);
+    }
   }
+
 }
+
